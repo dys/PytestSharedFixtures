fdaabfd6fc5038597eab66c8a72fde169aa4001d
Andrew Eaton <null@dys4ik.com>
2016-02-14 18:08:32 -0500

    removing unwanted files

--- tests/cli.py
+++
@@ -1,9 +0,0 @@
-cmd = """
-python -c 'from libraries.library_one import library_one
-l1 = library_one()
-print l1.config'
-"""
-print cmd
-from subprocess import check_output
-config = check_output(cmd, shell=True)
-print config
