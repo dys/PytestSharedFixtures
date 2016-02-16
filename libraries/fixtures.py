2e86afbc198f29f2794a5436af1907138f6d7c4b
Andrew Eaton <null@dys4ik.com>
2016-02-15 20:22:02 -0500

    Corrected a few tests

--- libraries/fixtures.py
+++ libraries/fixtures.py
@@ -1,23 +1,11 @@
 import pytest
-import functools
-
-def make_global(cls):
-    @functools.wraps(cls)
-    def wrapper(*args, **kwargs):
-        name = '_' + cls.__class__.__name__
-        try:
-            return globals()[name]
-        except KeyError:
-            globals()[name] = cls(*args, **kwargs)
-            return globals()[name]
-    return wrapper
 
 @pytest.fixture
 def library_one():
-    from libraries.library_one import library_one as wrapper
-    return wrapper
+    from libraries.library_one import library_one as builder
+    return builder
 
 @pytest.fixture
 def library_two():
-    from libraries.library_two import library_two as wrapper
-    return wrapper
+    from libraries.library_two import library_two as builder
+    return builder
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,6 +1,44 @@
-"""These tests contain example usage (and actual testing) of the POC for pytest fixture wrappers.
+"""
+A shared fixture is a python class that can be used normally and as a pytest fixture, and may depend on another fixture.
 
-This solution is intended to allow us to use our libraries as pytest fixtures or regular imports with a similar interface.
+    # As a fixture in a testcase:
+    def test_func(shared_fixture):
+
+    # As a regular import from another library:
+    from libraries import shared_fixture
+    l = shared_fixture()
+
+Users may want to configure one fixture before it is used by another fixture.
+This means we can't initialize fixtures before they've been pulled into a
+testcase.
+
+To solve this, the shared fixture has a method to get a singleton instance:
+
+    # shared_fixture.py
+    class SharedFixture(): # a typical class
+    def shared_fixture(): # returns singleton of SharedFixture
+
+And a matching pytest fixture is defined which calls this method:
+
+    # fixtures.py
+    @pytest.fixture
+    def shared_fixture(): # returns shared_fixture.py:shared_fixture()
+
+Other fixtures call this to get the shared fixture. If the shared fixture is
+not yet initialized they'll get one with defaults: otherwise, they'll get the
+existing one:
+
+    # Here we configure shared_fixture before other_fixture gets it
+    def test_func(shared_fixture, other_fixture):
+        sf = shared_fixture()
+        sf.option = "new_opt"
+        of = other_fixture()
+        assert of.sf.option == "new_opt" # other_fixture has a reference to shared_fixture
+
+    # This would use the default shared_fixture instead:
+    def test_func(other_fixture):
+        of = other_fixture()
+        assert of.sf.option == "default"
 """
 import libraries
 
@@ -12,22 +50,16 @@ def test_library_as_import():
     l1 = libraries.library_one.library_one()
     assert l1.config == "default"
 
-def test_library_from_cli():
-    cmd = 'python -c "from libraries.library_one import library_one; print library_one().config"'
-    from subprocess import check_output
-    config = check_output(cmd, shell=True).rstrip()
-    assert config == "default"
-
-def test_fixture_is_global():
-    a = libraries.fixtures.library_one()
-    b = libraries.fixtures.library_one()
+def test_fixture_is_global(library_one):
+    a = library_one()
+    b = libraries.library_one.library_one()
     assert a is b
 
-def test_fixture_dependency(library_two):
+def test_default_fixture_dependency(library_two):
     l2 = library_two()
     assert l2.l1.config == "default"
 
-def test_config_fixture_dependency(library_one, library_two):
+def test_configured_fixture_dependency(library_one, library_two):
     l1 = library_one()
     l1.config = "changed"
     l2 = library_two()
